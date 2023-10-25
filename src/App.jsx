b3befb930310f59bae1d35f2428f73f9daf674a8
Dev Pipalia <devpipalia003@gmail.com>
2023-10-26 00:36:39 +0530

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Button from '@mui/material/Button';
+import { TextField } from "@mui/material";
+function App() {
+
+
+  return (
+    <>
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </>
+  )
+}
+
+export default App
