80399581031ef68c43001c4513617344602ded3e
Dev Pipalia <devpipalia003@gmail.com>
2023-10-26 17:39:03 +0530

    page 2 with date picker

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function WelcomePage() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  // Calculate the graduation date
+  const graduationDate = new Date(selectedDate);
+  graduationDate.setFullYear(graduationDate.getFullYear() + 4);
+
   return (
-    <Container
-      maxWidth="xs"
+    <div
       style={{
-        background: '#F25B55',
-        padding: '20px',
-        borderRadius: '10px',
-        marginTop: '50px',
-        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
       }}
     >
-      <Typography variant="h4" gutterBottom>
-        Hello, World
-      </Typography>
-    </Container>
+      <h1 style={{ fontFamily: 'Poppins', fontSize: '32px' }}>Select your Joining Date</h1>
+      <Calendar
+        value={selectedDate}
+        onChange={handleDateChange}
+      />
+      <p style={{ fontSize: '24 px', margin: '20px 0',marginTop: '40px' }}>Your starting date: {selectedDate.toDateString()}</p>
+      <p style={{ fontSize: '24px', margin: '5px 0' }}>Your graduation date: {graduationDate.toDateString()}</p>
+    </div>
   );
 }
 
